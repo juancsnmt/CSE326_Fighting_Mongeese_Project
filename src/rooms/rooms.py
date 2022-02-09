69f42388c6645e08f4716e89855a8f94256d512d
millipedes <millipedes@earthlink.net>
2022-02-09 14:24:40 -0700

    update commit

--- src/rooms/rooms.py
+++ src/rooms/rooms.py
@@ -2,12 +2,21 @@ import paths.paths
 
 class rooms:
     def __init__(self, *args, **kwargs):
+        self.path_no = 0
         if 'above' in kwargs:
-            self.above = paths(kwargs.get('above'))
+            self.paths[self.path_no] = copy.deepcopy(kwargs.get('above'))
+            self.path_no += 1
+            self.above_index = self.path_no
         if 'right' in kwargs:
-            self.right = paths(kwargs.get('right'))
+            self.paths[self.path_no] = copy.deepcopy(kwargs.get('above'))
+            self.path_no += 1
+            self.right = copy.deepcopy(kwargs.get('right'))
         if 'below' in kwargs: 
-            self.below = paths(kwargs.get('below'))
+            self.paths[self.path_no] = copy.deepcopy(kwargs.get('above'))
+            self.path_no += 1
+            self.below = copy.deepcopypaths(kwargs.get('below'))
         if 'left' in kwargs:
-            self.left = paths(kwargs.get('left'))
+            self.paths[self.path_no] = copy.deepcopy(kwargs.get('above'))
+            self.path_no += 1            
+            self.left = copy.deepcopypaths(kwargs.get('left'))
  
